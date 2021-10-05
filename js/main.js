e8d1d42a7656851dfd742c0d35a1a1d7f3d81556
Tracey <traceylea9@gmail.com>
2021-10-06 09:35:56 +1100

    added psudo code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 console.log('Working...')
+
+/*
+- need yourTurn & winner etc. variables
+- create board with css - table, rows and cells.
+- player turn as a boolean O = true, X = false
+- take turn - click on cell
+- current player switches to next player and next player becomes the current player
+- error / admonishment if player clicks on a cell already in use (engaged?)
+- is the game over?
+- mark/change cell style (change class) to show as engaged
+- winner based on the cell state - check 4 directions (V, H, /, \)
+- no? are there empty cells? next player, else there is a tie
+*/
