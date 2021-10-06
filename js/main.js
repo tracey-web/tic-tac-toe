a22588bb91720ea2363e163d8375df5d3bc1d3b5
Tracey <traceylea9@gmail.com>
2021-10-07 09:23:03 +1100

    added google fonts links and created jQuery variables for game state

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 // Project0 - Tic Tac Toe
 
-/*
-- need yourTurn & winner etc. variables
-- create board with css - table, rows and cells.
-- player turn as a boolean O = true, X = false
-- take turn - click on cell
-- current player switches to next player and next player becomes the current player
-- error / admonishment if player clicks on a cell already in use (engaged?)
-- is the game over?
-- mark/change cell style (change class) to show as engaged
-- winner based on the cell state - check 4 directions (V, H, /, \)
-- no? are there empty cells? next player, else there is a tie
-*/
+/* 
+- need $your_turn & $winner etc. variables 
+- create board with css - table, rows and cells. 
+- player turn as a boolean O = true, X = false 
+- take turn - click on square 
+- current player switches to next player and next player becomes the current player 
+- error if player clicks on a cell already active 
+- is the game over? 
+- mark/change cell style (change class) to show as engaged 
+- $winner based on the cell state - check all 4 directions 
+- no winner? are there empty cells? next player, else there is a tie 
+*/ 
+
+$(document).ready(function(){
+
+  // Create board by selecting all of the td's into the variable 
+  const $squares = $("#board td"); 
+
+  // The game states 
+  let $your_turn = $("#turn"); 
+  let $winner = $("#winner"); 
+
+});
